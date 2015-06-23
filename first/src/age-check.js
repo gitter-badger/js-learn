11a558c33c91ec82c934ab3aee566607febb798b
nomean42 <ygreknomean@gmail.com>
2015-06-24 01:33:40 +0600

    perenes cod v fail

---
+++ first/src/age-check.js
@@ -0,0 +1,12 @@
+/**
+ * Created by igor on 24.06.15.
+ */
+var agecur = prompt('введи возраст');
+var age = parseInt(agecur, 10);
+if (age < 10) {
+    alert('уходи!');
+} else if (age > 70) {
+    alert('старый!');
+} else {
+    alert('привет!');
+}
