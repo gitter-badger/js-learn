8b4ff935f44097514f13bd0487eb78f395cf2dd1
nomean42 <ygreknomean@gmail.com>
2015-06-24 12:36:33 +0600

    test commit

---
+++ second/src/factorial-gen.js
@@ -0,0 +1,14 @@
+/**
+ * Created by YGREK on 24.06.2015.
+ */
+var factStart = prompt('введите начало диапазона'),
+    factS = parseInt(factStart, 10),
+    factEnd = prompt('введите конец диапазона'),
+    factE = parseInt(factEnd, 10);
+for (i = 1; i <= factS; i++) {
+    factS = factS * i;
+}
+for (i = factS; i <= factE; i++) {
+    factS = factS*i;
+    document.write (i + '! =' + factS + '<br>');
+}
