de4bf753016149b1aaa17975338eb45eeb047b20
nomean42 <ygreknomean@gmail.com>
2015-06-26 15:11:19 +0600

    try to correct factorial-gen.js

--- second/src/factorial-gen.js
+++ second/src/factorial-gen.js
@@ -1,16 +1,27 @@
 /**
  * Created by YGREK on 24.06.2015.
  */
-var factStart = prompt('������� ������ ���������'),
+var factStart = prompt('введите начало диапазона'),
     factS = parseInt(factStart, 10),
-    factEnd = prompt('������� ����� ���������'),
-    factE = parseInt(factEnd, 10);
-
-for (i = 1; i <= factS; i++) {
-    factS = factS * i;
+    factEnd = prompt('введите конец диапазона'),
+    factE = parseInt(factEnd, 10),
+    factOri = 1;
+for (i = 1; i < (factE - 1); ++i)
+    {
+    factOri = factOri * i;
+        document.write(i + '! =' + factOri + '<br>');
+    }
+    factS =  factS * factOr;
+  /**  (j = factS; j < factE; j++)
+{
+    factS = factS * j;
+    document.write(j + '! =' + factS + '<br>');
 }
+   /** factOri = factS * factE;
+    document.write(factE + '! =' + factOri + '<br>'); */
+    document.write(factS);
+/**document.write(i + '! =' + factS + '<br>');*/
 
-for (i = factS; i <= factE; i++) {
-    factS = factS * i;
-    document.write(i + '! =' + factS + '<br>');
-}
+
+
+
