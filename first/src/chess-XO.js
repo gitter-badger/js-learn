1a3f511086a948594c6913870e6ce9f3d08d13cd
nomean42 <ygreknomean@gmail.com>
2015-06-30 01:05:20 +0700

    try to make chess-xo.js work, still not working, decided to  close that project.

--- first/src/chess-XO.js
+++ first/src/chess-XO.js
@@ -1,3 +1,37 @@
 /**
  * Created by YGREK on 26.06.2015.
  */
+var str = '',
+    c = 8,
+    d = 8,
+    sc = 8;
+while (sc) {
+    if (sc) {
+        while (c) {
+            if (c % 2) {
+                str += '[X]';
+            }
+            else {
+                str += '[O]';
+            }
+            --c;
+        }
+        --sc;
+        str += '<br>';
+
+        while (d) {
+            if (d % 2) {
+                str += '[O]';
+            }
+            else {
+                str += '[X]';
+            }
+            --d;
+        }
+        str += '<br>';
+        --sc;
+    }
+    else {
+        document.write(str);
+    }
+}
